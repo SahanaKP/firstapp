7046b615f631b5da2a1940111562b7ed1fd3a93a
SahanaKP <sahanakp25@gmail.com>
2022-03-19 17:12:43 +0530

    arrays project added

---
+++ Arrays/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1.declare data type and array name and size
+    int book[5];
+
+    //2. add array elements also called array data
+
+    book[0]= 10;
+    book[1]=20;
+    book[2]=30;
+    book[3]=40;
+    book[4]=50;
+    //3. display array data one by one using cout
+    // we can use for loop to display array data
+
+    cout<<book[0]<<endl;
+    cout<<book[1]<<endl;
+    cout<<book[2]<<endl;
+    cout<<book[3]<<endl;
+    cout<<book[4]<<endl;
+    return 0;
+}
